1a965dbf7df7e8a81169b715ca46545a188db868
meldianing <35475106+meldianing@users.noreply.github.com>
2019-01-03 18:10:46 -0800

    Delete library diagram.php

--- library diagram.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<title>HTML Tutorial</title>
-<body>
-<div href="https://drive.google.com/file/d/11QwzpTJQusjObCbxH_f756KVBRM6PcJR/view?usp=sharing"></div>
-
-</body>
-</html>
